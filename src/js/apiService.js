e4e0cdea3e1a5798a7be3481babba618e9bb2cee
Marina Striukova <65074360+marinastriukova@users.noreply.github.com>
2020-11-24 17:50:26 +0200

    -

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,3 +1,18 @@
+const API_KEY = '19199045-2bc94f1b29c918ae7d7bc7dd7';
+const BASE_URL = 'https://pixabay.com/api';
+const options = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
+
+export default class NewApiService {
+  constructor(){
+      this.searchQuery = '';
+      this.page = null;
+  }
+
+  fetchGallery() {
       const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
   
       return fetch(url)
